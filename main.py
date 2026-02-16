0bcc02119fcae547a019398e9de1b920c9e87a9b
Marko Stankovic <mstankovicc997@gmail.com>
2026-02-16 04:43:09 +0100

    Update products and remove inventory output from main

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from product_manager import ProductManager
 
 manager = ProductManager()
 
-p1 = Product("Laptop", 1000, 5)
-p2 = Product("Telefon", 500, 10)
+p1 = Product("Laptop Pro", 1200, 3)
+p2 = Product("Telefon X", 700, 5)
 p3 = Product("Mis", 20, 50)
+
 
 manager.add_product(p1)
 manager.add_product(p2)
 manager.add_product(p3)
 
-manager.show_products()
-print("Ukupna vrednost inventara:", manager.total_inventory_value())
